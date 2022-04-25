8526d11ebcdf93e17f0c381d61d656b2e08310bd
Andreas Notokuusmo <andreasnotokusumo23@gmail.com>
2022-04-26 06:45:48 +0700

    :sparkles: InvoiceEvent Bloc Update, Invoice JSON Model Fixed

--- lib/app/invoice/bloc/invoice_bloc.dart
+++ lib/app/invoice/bloc/invoice_bloc.dart
@@ -1,5 +1,8 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:sindu_store/model/invoice/invoice.dart';
+import 'package:sindu_store/model/invoice/invoice_status.dart';
+import 'package:sindu_store/model/product/product_discount.dart';
 
 part 'invoice_event.dart';
 part 'invoice_state.dart';
--- lib/app/invoice/bloc/invoice_event.dart
+++ lib/app/invoice/bloc/invoice_event.dart
@@ -7,4 +7,42 @@ abstract class InvoiceEvent extends Equatable {
   List<Object> get props => [];
 }
 
+class InvoiceEventActivate extends InvoiceEvent {
+  final Invoice invoice;
+  final String initialProductID;
+
+  const InvoiceEventActivate({required this.invoice, required this.initialProductID});
+}
+
+class InvoiceEventAddItem extends InvoiceEvent {
+  final Invoice invoice;
+  final String productID;
+
+  const InvoiceEventAddItem({required this.invoice, required this.productID});
+}
+
+class InvoiceEventRemoveItem extends InvoiceEvent {
+  final Invoice invoice;
+  final String productID;
+
+  const InvoiceEventRemoveItem({required this.invoice, required this.productID});
+}
+
+class InvoiceEventEditDiscount extends InvoiceEvent {
+  final Invoice invoice;
+  final String productID;
+  final ProductDiscount productDiscount;
+
+  const InvoiceEventEditDiscount({required this.invoice, required this.productID, required this.productDiscount});
+}
+
+class InvoiceEventMarkStatus extends InvoiceEvent {
+  final Invoice invoice;
+  final InvoiceStatus status;
+
+  const InvoiceEventMarkStatus({required this.invoice, required this.status});
+}
+
+class InvoiceEventDeactivate extends InvoiceEvent {}
+
 
